7548746a5861a0c65299fe63a9f2f2992bc1b716
Felix Tubiana <felixtubiana@gmail.com>
2016-06-12 18:41:11 +0800

    Refactored create_graph.R

--- create_graph.R
+++ create_graph.R
@@ -1,15 +1,73 @@
-args <- commandArgs(trailingOnly = TRUE)
+# R script to create a graph of code changes per day vs time
+#
+# usage: Rscript create_graph.R data.csv
+#
+
+# Tweakable Parameters
+minimum_commits                  <- 10
+n_days_to_filter                 <- 11
+minimum_changes_for_author_label <- 1000
+author_label_cex                 <- 0.4
+points_character                 <- 20
+points_character_cex             <- 0.05
+
+# Parse command line options
+args <- commandArgs( trailingOnly = TRUE )
 filename = args[1]
 
-d <- read.csv(filename, head = TRUE, sep=":")
+# Fetch data
+d <- read.csv( filename, head = TRUE, sep=":" )
 
-d$date <- as.Date(d$date, "%Y-%m-%d")
+d$date <- as.Date( d$date, "%Y-%m-%d" )
+
+nauthors <- length( unique( d$author ) )
+
+dd <- vector( mode="list", length=nauthors )
+ic <- 1
+for ( author in unique( d$author ) ) {
+    ii <- d$author == author
+    xx <- d$date[ii]
+
+    # Skip users with few commits
+    if( length(xx) < minimum_commits ) next
+
+    # Median changes per day
+    median_changes <- median( diff( d$total[ii] ) )
+
+    # d$total still may have wild jumps if we missed any
+    # changes that should obviously be ignored
+    # ( rename directories, include javascript libraries, perltidy whole files, etc )
+    # diff of d$total gives us a vector of changes per day
+    # runmed(...,11) of the result applies an 11-day median filter to smooth it out
+    # cumsum adds it all up, so it is a cumulative vector like the original d$total
+    yy <- cumsum( runmed( diff( d$total[ii] ), n_days_to_filter ) )
+
+    # xx[-1] to shorten xx by one, so it is same length as yy
+    dd[[ic]]$x      <- xx[-1]
+    dd[[ic]]$y      <- yy
+    dd[[ic]]$author <- author
+    dd[[ic]]$cpd    <- median_changes
+
+    ic <- ic + 1
+}
+
+filled <- !unlist( lapply( dd, is.null ) )
+iauthors <- ( 1:nauthors )[filled]
+C <- rainbow( length( iauthors ) )
+
+ymax <- 0
+for ( i in c( iauthors ) ) {
+    ymax <- max( ymax, max( dd[[i]]$y ) )
+}
+
+# leave room for labels
+xlimit <- c(min(d$date), max(d$date) + 1.5*365)
+
+# Round up to nearest 0.1 of the significant digit
+significant <- floor( log10( ymax ) ) - 1
+ymax <- 10**significant * ceiling( ymax / 10**significant )
 
 pdf(file="git_rockstar.pdf")
-# leave room for labels
-xlimit <- c(min(d$date), max(d$date) + 2*365)
-# arbitrary, but allows good comparison between best/worst
-ymax <- 100000
 
 plot( xlimit, c(NaN,NaN),
       ylim=c(1,ymax),
@@ -23,57 +81,49 @@ plot( xlimit, c(NaN,NaN),
       xaxt="n",
 )
 
-# TODO: dynamic tick labels
-xt  <- c(  0, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000)
-xtl <- c("0", "10k", "20k", "30k", "40k" ,"50k", "60k", "70k", "80k", "90k", "100k")
+xt  <- seq(0, ymax, 10**significant)
+xtl <- xt
+for ( i in c(1:length(xt)) ) {
+    value <- xt[i]
+    label <- ifelse( value == 0, "0",
+             ifelse( value < 10^3,  value,
+             ifelse( value < 10^6,  paste( value/10^3,  "k", sep="" ),
+             ifelse( value < 10^9,  paste( value/10^6,  "m", sep="" ),
+             ifelse( value < 10^12, paste( value/10^12, "b", sep="" ),
+             ifelse( value < 10^15, paste( value/10^15, "t", sep="" ), "Infinity"
+    ))))))
+    xtl[i] <- label
+}
+
 # Custom y-ticks and labels, las=2 means tick labels perpendicular to axis
 axis(2, at=xt, labels=xtl, las=2)
 
-yt <- as.Date(paste(2000:2017, "-01-01", sep=''))
-axis(1, at=yt,labels=c(2000:2017), las=2)
+years <- as.numeric( substr( as.character( xlimit ), 1, 4 ) )
+# Ensure there will be at least 2 tick marks
+years[2] = ifelse( years[2] == years[1], years[2] + 1, years[2] )
 
-# Colors
-C <- rainbow(length(unique(d$author)))
+yvec <- years[1]:years[2]
+yt <- as.Date( paste( yvec, "-01-01", sep='' ) )
+axis(1, at=yt, labels=yvec, las=2)
 
-ic <- 0
-for ( author in unique(d$author) ) {
-    ic <- ic + 1
-#    print(author)
-    ii <- d$author == author
-    xx <- d$date[ii]
-    # Median changes per day
-    median_changes <- median(diff(d$total[ii]))
+for ( i in c(iauthors) ) {
+    xx     <- dd[[i]]$x
+    yy     <- dd[[i]]$y
+    author <- dd[[i]]$author
+    cpd    <- dd[[i]]$cpd
 
-    # d$total still may have wild jumps if we missed any 
-    # changes that should obviously be ignored
-    # ( rename directories, include javascript libraries, perltidy whole files, etc )
-    # diff of d$total gives us a vector of changes per day
-    # runmed(...,11) of the result applies an 11-day median filter to smooth it out
-    # cumsum adds it all up, so it is a cumulative vector like the original d$total
-    yy <- cumsum(runmed(diff(d$total[ii]),11))
-
-    # handle if an author is off the chart, so we can label him
-    last_on_graph <- length(yy[ yy < ymax ])
+    # col sets the color, pch sets the marker, cex alters the marker size
+    points( xx, yy, col=C[i], pch=points_character, cex=points_character_cex )
     
-    # Skip anyone with fewer than 10 days of commits
-    if (length(xx) > 10) {
-
-        # xx[-1] to shorten xx by one, so it is same length as yy
-        # col sets the color, pch sets the marker, cex alters the marker size
-        points( xx[-1], yy, col=C[ic], pch=20, cex=0.05 )    
-        if ( last_on_graph > 0 ) {
-
-            # Do not bother labelling authors with fewer than 1000 total changes.
-            if (yy[last_on_graph] > 1000) {
-                # pos=4 means align left
-                text( xx[last_on_graph + 1], yy[last_on_graph],
-                     sprintf("%s [ %s cpd ]", author, median_changes),
-                     pos=4,
-                     cex=0.2,
-                     col=C[ic])
-            }
-        }
+    # Do not bother labelling authors with fewer than 1000 total changes.
+    if (max(yy) > minimum_changes_for_author_label ) {
+        # pos=4 means align left
+        text( xx[ length(xx) ], yy[ length(yy) ],
+             sprintf("%s [ %s cpd ]", author, cpd),
+             pos=4,
+             cex=author_label_cex,
+             col=C[ic])
     }
 }
 
-dev.off();
+dev.off()
